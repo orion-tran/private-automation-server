3bf8322351a2598d0561307245911b6e5e2c8e25
Orion Tran <oriontrandev@gmail.com>
2024-08-09 04:29:45 -0400

    feat: log on panic

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::env;
 use anyhow::Result;
 use dotenvy::dotenv;
 use tracing::info;
+use tracing_panic::panic_hook;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
     tracing_subscriber::fmt::init();
+    std::panic::set_hook(Box::new(panic_hook));
 
     info!("version {}", env!("CARGO_PKG_VERSION"));
     info!(
