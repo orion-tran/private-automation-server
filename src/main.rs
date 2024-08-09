1574025798672e9ef0643c62893a008e3c251a50
Orion Tran <oriontrandev@gmail.com>
2024-08-09 04:23:14 -0400

    feat: project bootstrap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use anyhow::Result;
+use dotenvy::dotenv;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    info!("version {}", env!("CARGO_PKG_VERSION"));
+    info!(
+        arch = env::consts::ARCH,
+        family = env::consts::FAMILY,
+        os = env::consts::OS,
+    );
+
+    Ok(())
 }
